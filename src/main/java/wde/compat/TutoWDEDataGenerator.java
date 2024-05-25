87fa43386a626fb4a9ddee354da12631175a44b3
Alysscendre <alyssandredesveaux@gmail.com>
2024-05-25 18:02:10 +0200

    Finally building

---
+++ src/main/java/wde/compat/TutoWDEDataGenerator.java
@@ -0,0 +1,11 @@
+package wde.compat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutoWDEDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/wde/compat/WDEtutoClient.java
@@ -0,0 +1,10 @@
+package wde.compat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WDEtutoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
